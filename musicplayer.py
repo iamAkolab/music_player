996e9464010b4d0645fadfa26308b4a8f83c599c
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-04-07 23:53:48 +0100

    pygame installation

---
+++ musicplayer.py
@@ -0,0 +1,8 @@
+import pygame
+import tkinter as tkr
+import tkinter.filedialog import askdirectory
+import os
+
+musicplayer = tkr.TK()
+musicplayer.title("Music Player")
+musicplayer.geometry("450 x 350")
