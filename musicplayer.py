d07b23ace4e82de1ff537dfe23f80bd187bc2633
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-04-08 00:17:57 +0100

    imported all the classes........

--- musicplayer.py
+++ musicplayer.py
@@ -1,8 +1,22 @@
 import pygame
 import tkinter as tkr
-import tkinter.filedialog import askdirectory
+from tkinter.filedialog import askdirectory
 import os
+
 
 musicplayer = tkr.TK()
 musicplayer.title("Music Player")
 musicplayer.geometry("450 x 350")
+
+directory = askdirectory()
+os.chdir(directory)
+songlist = os.listdir()
+playlist = tkr.Listbox(musicplayer, font ="Helvetica 12 bold", bg="yellow", selectmode = tkr.SINGLE)
+
+for item in songlist:
+    pos = 0
+    playlist.insert(pos, item)
+    pos = pos + 1
+
+pygame.init()
+pygame.mixer.init()
