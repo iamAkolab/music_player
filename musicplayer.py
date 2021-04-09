4ef3e83204026f35949e658c075c3c662ec8d980
iamAkolab <77028341+iamAkolab@users.noreply.github.com>
2021-04-10 00:03:46 +0100

    added ppack..........

--- musicplayer.py
+++ musicplayer.py
@@ -35,7 +35,19 @@ def pause():
 def unpause():
     pygame.mixer.music.unpause()
 
-Button1 - tkr.Button(musicplayer, width = 5, height = 3, font = "Helvetica 12 bold", text = "PLAY", bg = "red", fg = "white")
-Button2 - tkr.Button(musicplayer, width = 5, height = 3, font = "Helvetica 12 bold", text = "PLAY", bg = "purple", fg = "white")
-Button3 - tkr.Button(musicplayer, width = 5, height = 3, font = "Helvetica 12 bold", text = "PLAY", bg = "green, fg = "white")
-Button4 - tkr.Button(musicplayer, width = 5, height = 3, font = "Helvetica 12 bold", text = "PLAY", bg = "blue", fg = "white")
+Button1 = tkr.Button(musicplayer, width = 5, height = 3, font = "Helvetica 12 bold", text = "PLAY", bg = "red", fg = "white")
+Button2 = tkr.Button(musicplayer, width = 5, height = 3, font = "Helvetica 12 bold", text = "PLAY", bg = "purple", fg = "white")
+Button3 = tkr.Button(musicplayer, width = 5, height = 3, font = "Helvetica 12 bold", text = "PLAY", bg = "green", fg = "white")
+Button4 = tkr.Button(musicplayer, width = 5, height = 3, font = "Helvetica 12 bold", text = "PLAY", bg = "blue", fg = "white")
+
+var = tkr.StringVar()
+songtitle = tkr.Label(musicplayer, font = "Helvetica 12 bold", textvar = var)
+
+songtitle.pack()
+Button1.pack(fill = "x")
+Button2.pack(fill = "x")
+Button3.pack(fill = "x")
+Button4.pack(fill = "x")
+playlist.pack(fill = "both", expand = "yes")
+
+musicplayer.mainloop()
